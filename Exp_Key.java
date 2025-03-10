2a2b81e95ea4603dd9688c4017b41ccc6b6440b9
rafiki04 <cris_rgj13@hotmail.com>
2025-03-09 23:59:28 -0600

    primero

---
+++ Exp_Key.java
@@ -0,0 +1,14 @@
+public class Exp_Key {
+    int N_R = 10;
+    String key ="";
+    public static void main(String[] args) {
+        
+        while(N_R%4 ==0){
+
+        }
+        
+    }
+    
+
+    
+}
